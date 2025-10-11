ad6e60fa7acf2614a5ba9d417edcfe1eb67e53fd
Youssef Ait Said <saidyoussef642@gmail.com>
2025-10-11 21:29:44 +0100

    Methode par fichier config.xml

--- src/main/java/pres/Pres1.java
+++ src/main/java/pres/Pres1.java
@@ -5,13 +5,27 @@ import dao.DaoImplV2;
 import dao.IDao;
 import metier.IMetier;
 import metier.ImetierImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Pres1 {
-    public static void main(String[] args) {
-        IDao dao = new DaoImplV2();
-        ImetierImpl metier = new ImetierImpl();
-        metier.setDao(dao);
+
+    public static void main(String[] args) throws Exception {
+ // methode par fichier config.xml:
+        ApplicationContext contex = new ClassPathXmlApplicationContext("config.xml");
+
+        IMetier metier =  (IMetier) contex.getBean("met"); // getBean() => prend comme parammetre  le id du bean qu on veurt creer.
+//        IDao dao = new DaoImpl();
+//        ImetierImpl metier = new ImetierImpl();
+//        metier.setDao(dao);add 
 
         System.out.println("Resultat :" + metier.calcul());
+
+
+
     }
 }
+
+
+
